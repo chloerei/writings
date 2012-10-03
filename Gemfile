3c5065feaf96345b52256d081580f6e4b1e508f8
Rei <chloerei@gmail.com>
2012-10-03 16:03:00 +0800

    init

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development do
+  gem 'guard-livereload'
+  gem 'rack-livereload'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Publish::Application.config.secret_token = 'cd5330c10e83a9c91b4ba5011ebbcbe323fa24c0e1861cefad743da844148c18201d9006fbad534b8ec438e87cb1105a447047b033299f51b5f7e7369480a06e'
