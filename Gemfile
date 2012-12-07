ecb16178e2c0b79d870cebeec8b0e73b7322340c
Rei <chloerei@gmail.com>
2012-12-07 17:52:40 +0800

    Add mongoid, APP_CONFIG

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
 
-gem 'sqlite3'
+gem 'mongoid'
 gem 'slim'
 gem 'redcarpet'
 
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -51,7 +54,7 @@ module Publish
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -66,3 +69,5 @@ module Publish
     end
   end
 end
+
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
