a6277100619da8ce8e41c0076c8435f3bcd6aafc
Rei <chloerei@gmail.com>
2012-12-10 11:56:57 +0800

    remove unuse validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,36 +11,6 @@ class User
 
   has_secure_password
 
-  class ConfirmationValidator < ActiveModel::EachValidator # :nodoc:
-    def initialize(options)
-      options[:attributes] = options[:attributes].map { |attribute| "#{attribute}_confirmation" }
-      super
-    end
-
-    def validate_each(record, attribute, value)
-      attribute_to_confirm = attribute.to_s.sub('_confirmation', '')
-      confirmed = record.send(attribute_to_confirm)
-      if value != confirmed
-        human_attribute_name = record.class.human_attribute_name(attribute_to_confirm)
-        record.errors.add(attribute, :confirmation, options.merge(:attribute => human_attribute_name))
-      end
-    end
-
-    def client_side_hash(model, attribute, force = nil)
-      attribute_to_confirm = attribute.to_s.split(/_confirmation/)[0]
-      human_attribute_name = model.class.human_attribute_name(attribute_to_confirm)
-      build_client_side_hash(model, attribute, self.options.dup.merge(:attribute => human_attribute_name))
-    end
-
-    def setup(klass)
-      klass.send(:attr_accessor, *attributes.map do |attribute|
-        attribute unless klass.method_defined?(attribute)
-      end.compact)
-    end
-  end
-
-  validates :password, :confirmation => true
-
   validates :name, :email, :presence => true, :uniqueness => {:case_sensitive => false}
   validates :name, :format => {:with => /\A\w+\z/, :message => 'only A-Z, a-z, _ allowed'}, :length => {:in => 3..20}
   validates :email, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
