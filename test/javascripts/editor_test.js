a2ad210b308c24ff996a76381e89fc5c06e159a8
Rei <chloerei@gmail.com>
2012-12-28 21:39:06 +0800

    add sanitizeTest funciton, add div to p test.

--- test/javascripts/editor_test.js
+++ test/javascripts/editor_test.js
@@ -11,28 +11,35 @@ test("create", function() {
   ok(editor.editable.length);
 });
 
+var sanitizeTest = function(html, expected) {
+  editor.editable.html(html);
+  editor.sanitize();
+  equal(editor.editable.html(), expected);
+};
+
 test("sanitize p", function() {
   // flatten nested p
-  editor.editable.html(
-    $('<p>').append($('<p>').text('line one')).append($('<p>').text('line two'))
-  );
-  editor.sanitize();
-  equal(editor.editable.html(),
-    '<p>line one</p>' +
-    '<p>line two</p>'
+  sanitizeTest(
+    $('<p>').append($('<p>').text('line one')).append($('<p>').text('line two')),
+    '<p>line one</p><p>line two</p>'
   );
 
   // stript nested p if has text node
-  editor.editable.html(
-    $('<p>').text('text').append($('<p>').text('nested'))
+  sanitizeTest(
+    $('<p>').text('text').append($('<p>').text('nested')),
+    '<p>textnested</p>'
   );
-  editor.sanitize();
-  equal(editor.editable.html(), '<p>textnested</p>');
 
   // stript more nested block
-  editor.editable.html(
-    $('<p>').text('text').append($('<h1>').append($('<p>').text('nested')))
+  sanitizeTest(
+    $('<p>').text('text').append($('<h1>').append($('<p>').text('nested'))),
+    '<p>textnested</p>'
   );
-  editor.sanitize();
-  equal(editor.editable.html(), '<p>textnested</p>');
+});
+
+test("div to p", function() {
+  sanitizeTest(
+    '<div>text</div>',
+    '<p>text</p>'
+  );
 });
