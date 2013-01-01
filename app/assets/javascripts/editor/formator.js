33d6fbed262fd730b1add2a4e8749435e58e470d
Rei <chloerei@gmail.com>
2013-01-01 15:42:30 +0800

    more formator logic.

--- app/assets/javascripts/editor/formator.js
+++ app/assets/javascripts/editor/formator.js
@@ -4,15 +4,180 @@ Editor.Formator = function(editable) {
 
 Editor.Formator.prototype = {
   isBold: function() {
-    return document.queryCommandValue('bold') === 'true';
+    return this.canBold() && document.queryCommandValue('bold') === 'true';
   },
 
   canBold: function() {
-    return !this.isWraped('h1, h2, h3, h4');
+    return !this.isWraped('h1, h2, h3, h4, code');
   },
 
   bold: function() {
-    this.exec('bold');
+    if (this.canBold()) {
+      this.exec('bold');
+    }
+  },
+
+  isItalic: function() {
+    return this.canItalic() && document.queryCommandValue('italic') === 'true';
+  },
+
+  canItalic: function() {
+    return !this.isWraped('code');
+  },
+
+  italic: function() {
+    if (this.canItalic()) {
+      this.exec('italic');
+    }
+  },
+
+  isStrikeThrough: function() {
+    return this.canItalic() && document.queryCommandValue('strikeThrough') === 'true';
+  },
+
+  canStrikeThrough: function() {
+    return !this.isWraped('code');
+  },
+
+  strikeThrough: function() {
+    if (this.canStrikeThrough()) {
+      this.exec('strikeThrough');
+    }
+  },
+
+  isUnderline: function() {
+    return this.canItalic() && document.queryCommandValue('underline') === 'true';
+  },
+
+  canUnderline: function() {
+    return !this.isWraped('code');
+  },
+
+  underline: function() {
+    if (this.canUnderline()) {
+      this.exec('underline');
+    }
+  },
+
+  isOrderedList: function() {
+    return this.canOrderedList() && document.queryCommandValue('insertOrderedList') === 'true';
+  },
+
+  canOrderedList: function() {
+    return !this.isWraped('h1, h2, h3, h4, code');
+  },
+
+  orderedList: function() {
+    if (this.canUnderline()) {
+      this.exec('insertOrderedList');
+    }
+  },
+
+  isUnorderedList: function() {
+    return this.canUnorderedList() && document.queryCommandValue('insertUnorderedList') === 'true';
+  },
+
+  canUnorderedList: function() {
+    return !this.isWraped('h1, h2, h3, h4, code');
+  },
+
+  unorderedList: function() {
+    if (this.canUnderline()) {
+      this.exec('insertUnorderedList');
+    }
+  },
+
+  isLink: function() {
+    return this.canLink() && document.queryCommandValue('createLink') === 'true';
+  },
+
+  canLink: function() {
+    return !this.isWraped('code');
+  },
+
+  link: function() {
+    var url = prompt('Link url:', 'http://');
+    if (url !== null && url !== '') {
+      this.exec('createLink', url);
+    }
+  },
+
+  isH1: function() {
+    return this.isWraped('h1');
+  },
+
+  canH1: function() {
+    return !this.isWraped('li, code');
+  },
+
+  h1: function() {
+    if (this.canH1()) {
+      this.formatHeader('h1');
+    }
+  },
+
+  isH2: function() {
+    return this.isWraped('h2');
+  },
+
+  canH2: function() {
+    return !this.isWraped('li, code');
+  },
+
+  h2: function() {
+    if (this.canH2()) {
+      this.formatHeader('h2');
+    }
+  },
+
+  isH3: function() {
+    if (this.canH3()) {
+      return this.isWraped('h3');
+    }
+  },
+
+  canH3: function() {
+    return !this.isWraped('li, code');
+  },
+
+  h3: function() {
+    if (this.canH3()) {
+      this.formatHeader('h3');
+    }
+  },
+
+  isH4: function() {
+    return this.isWraped('h4');
+  },
+
+  canH4: function() {
+    return !this.isWraped('li, code');
+  },
+
+  h4: function() {
+    if (this.canH4()) {
+      this.formatHeader('h4');
+    }
+  },
+
+  isP: function() {
+    return this.isWraped('p');
+  },
+
+  canP: function() {
+    return !this.isWraped('li, code');
+  },
+
+  p: function() {
+    this.exec('formatBlock', 'p');
+  },
+
+  formatHeader: function(type) {
+    if (document.queryCommandValue('formatBlock') === type) {
+      this.p();
+    } else {
+      this.exec('formatBlock', type);
+    }
   },
 
   isWraped: function(selector) {
--- test/javascripts/editor/formator_test.js
+++ test/javascripts/editor/formator_test.js
@@ -6,7 +6,7 @@ module("Editor.formator", {
   }
 });
 
-test("should check bold status", function() {
+test("should format bold", function() {
   this.formator.editable.html('<p>text</p>');
   document.getSelection().selectAllChildren(this.formator.editable.find('p')[0]);
   equal(this.formator.isBold(), false);
@@ -21,4 +21,167 @@ test("should check bold status", function() {
   equal(this.formator.editable.html(), '<p>text</p>');
   equal(this.formator.isBold(), false);
   equal(this.formator.canBold(), true);
+
+  // don't bold header
+  this.formator.editable.html('<h1>header</h1>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('h1')[0]);
+  equal(this.formator.isBold(), false);
+  equal(this.formator.canBold(), false);
+  this.formator.bold();
+  equal(this.formator.editable.html(), '<h1>header</h1>');
+});
+
+test("should format italic", function() {
+  this.formator.editable.html('<p>text</p>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('p')[0]);
+  equal(this.formator.isItalic(), false);
+  equal(this.formator.canItalic(), true);
+
+  this.formator.italic();
+  equal(this.formator.editable.html(), '<p><i>text</i></p>');
+  equal(this.formator.isItalic(), true);
+  equal(this.formator.canItalic(), true);
+
+  this.formator.italic();
+  equal(this.formator.editable.html(), '<p>text</p>');
+  equal(this.formator.isItalic(), false);
+  equal(this.formator.canItalic(), true);
+
+  // don't italic code
+  this.formator.editable.html('<code>code</code>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('code')[0]);
+  equal(this.formator.isItalic(), false);
+  equal(this.formator.canItalic(), false);
+  this.formator.italic();
+  equal(this.formator.editable.html(), '<code>code</code>');
+});
+
+test("should format strikeThrough", function() {
+  this.formator.editable.html('<p>text</p>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('p')[0]);
+  equal(this.formator.isStrikeThrough(), false);
+  equal(this.formator.canStrikeThrough(), true);
+
+  this.formator.strikeThrough();
+  equal(this.formator.editable.html(), '<p><strike>text</strike></p>');
+  equal(this.formator.isStrikeThrough(), true);
+  equal(this.formator.canStrikeThrough(), true);
+
+  this.formator.strikeThrough();
+  equal(this.formator.editable.html(), '<p>text</p>');
+  equal(this.formator.isStrikeThrough(), false);
+  equal(this.formator.canStrikeThrough(), true);
+
+  // don't strikeThrough code
+  this.formator.editable.html('<code>code</code>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('code')[0]);
+  equal(this.formator.isStrikeThrough(), false);
+  equal(this.formator.canStrikeThrough(), false);
+  this.formator.strikeThrough();
+  equal(this.formator.editable.html(), '<code>code</code>');
+});
+
+test("should format underline", function() {
+  this.formator.editable.html('<p>text</p>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('p')[0]);
+  equal(this.formator.isUnderline(), false);
+  equal(this.formator.canUnderline(), true);
+
+  this.formator.underline();
+  equal(this.formator.editable.html(), '<p><u>text</u></p>');
+  equal(this.formator.isUnderline(), true);
+  equal(this.formator.canUnderline(), true);
+
+  this.formator.underline();
+  equal(this.formator.editable.html(), '<p>text</p>');
+  equal(this.formator.isUnderline(), false);
+  equal(this.formator.canUnderline(), true);
+
+  // don't underline code
+  this.formator.editable.html('<code>code</code>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('code')[0]);
+  equal(this.formator.isUnderline(), false);
+  equal(this.formator.canUnderline(), false);
+  this.formator.underline();
+  equal(this.formator.editable.html(), '<code>code</code>');
+});
+
+test("should format orderedList", function() {
+  this.formator.editable.html('<p>text<br></p>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('p')[0]);
+  equal(this.formator.isOrderedList(), false);
+  equal(this.formator.canOrderedList(), true);
+
+  this.formator.orderedList();
+  equal(this.formator.editable.html(), '<p><ol><li>text<br></li></ol></p>');
+  equal(this.formator.isOrderedList(), true);
+  equal(this.formator.canOrderedList(), true);
+
+  this.formator.orderedList();
+  equal(this.formator.editable.html(), '<p>text<br></p>');
+  equal(this.formator.isOrderedList(), false);
+  equal(this.formator.canOrderedList(), true);
+
+  // don't orderedList code
+  this.formator.editable.html('<code>code</code>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('code')[0]);
+  equal(this.formator.isOrderedList(), false);
+  equal(this.formator.canOrderedList(), false);
+  this.formator.orderedList();
+  equal(this.formator.editable.html(), '<code>code</code>');
+});
+
+
+test("should format orderedList", function() {
+  this.formator.editable.html('<p>text<br></p>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('p')[0]);
+  equal(this.formator.isUnorderedList(), false);
+  equal(this.formator.canUnorderedList(), true);
+
+  this.formator.unorderedList();
+  equal(this.formator.editable.html(), '<p><ul><li>text<br></li></ul></p>');
+  equal(this.formator.isUnorderedList(), true);
+  equal(this.formator.canUnorderedList(), true);
+
+  this.formator.unorderedList();
+  equal(this.formator.editable.html(), '<p>text<br></p>');
+  equal(this.formator.isUnorderedList(), false);
+  equal(this.formator.canUnorderedList(), true);
+
+  // don't unorderedList code
+  this.formator.editable.html('<code>code</code>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('code')[0]);
+  equal(this.formator.isUnorderedList(), false);
+  equal(this.formator.canUnorderedList(), false);
+  this.formator.unorderedList();
+  equal(this.formator.editable.html(), '<code>code</code>');
+});
+
+/*
+ * TODO link test
+ */
+
+test("format header", function() {
+  this.formator.editable.html('<p>text</p>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('p')[0]);
+  equal(this.formator.isH1(), false);
+  equal(this.formator.canH1(), true);
+
+  this.formator.h1();
+  equal(this.formator.editable.html(), '<h1>text</h1>');
+  equal(this.formator.isH1(), true);
+  equal(this.formator.canH1(), true);
+
+  this.formator.h1();
+  equal(this.formator.editable.html(), '<p>text</p>');
+  equal(this.formator.isH1(), false);
+  equal(this.formator.canH1(), true);
+
+  // don't unorderedList code
+  this.formator.editable.html('<code>code</code>');
+  document.getSelection().selectAllChildren(this.formator.editable.find('code')[0]);
+  equal(this.formator.isUnorderedList(), false);
+  equal(this.formator.canUnorderedList(), false);
+  this.formator.unorderedList();
+  equal(this.formator.editable.html(), '<code>code</code>');
 });
