e327eae00278626e2ef5c1c6c32ce41349d7e075
Rei <chloerei@gmail.com>
2013-01-02 15:45:18 +0800

    more dropdown style.

--- app/assets/javascripts/sitewide/dropdown.js
+++ app/assets/javascripts/sitewide/dropdown.js
@@ -1,7 +1,13 @@
 $(document).on('click', '.dropdown-toggle', function(event) {
   event.preventDefault();
   event.stopPropagation();
-  $(this).closest('.dropdown').toggleClass('actived');
+
+  var $dropdown = $(this).closest('.dropdown');
+  var isOpened = $dropdown.hasClass('actived');
+  $('.dropdown').removeClass('actived');
+  if (!isOpened) {
+    $dropdown.addClass('actived');
+  }
 }).on('click', function(event) {
   $('.dropdown').removeClass('actived');
 });
