8f805f9b3cb018a12af68c395281967655dd7662
Rei <chloerei@gmail.com>
2013-01-17 14:12:24 +0800

    book show page.

--- app/controllers/site/books_controller.rb
+++ app/controllers/site/books_controller.rb
@@ -2,4 +2,9 @@ class Site::BooksController < Site::BaseController
   def index
     @books = @user.books.desc(:updated_at)
   end
+
+  def show
+    @book = @user.books.find_by :urlname => params[:id]
+    @articles = @book.articles.desc(:created_at).page(params[:page])
+  end
 end
