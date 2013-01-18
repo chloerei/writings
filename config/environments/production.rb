c77de0e91d76bc13450f5e0c6b990093ea9361a8
Rei <chloerei@gmail.com>
2013-01-18 12:04:03 +0800

    add precompile config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Publish::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( dashboard.js site.js )
+  config.assets.precompile += %w( dashboard.css site.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
