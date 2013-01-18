45ca61d4d88b98f4a907838d6a236ba9fa4ad5f4
Rei <chloerei@gmail.com>
2013-01-18 12:02:37 +0800

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :branch, "master"
 set :deploy_via, :remote_cache
 
 set :user, "webuser"
-set :deploy_to, "~/#{application}"
+set :deploy_to, "/home/webuser/#{application}"
 set :use_sudo, false
 
 role :web, "chloerei.com"
@@ -21,7 +21,7 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 
   task :copy_config do
