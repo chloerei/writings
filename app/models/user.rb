59f6c03e9e2cb2176998ba3f53372aa6add894d9
Rei <chloerei@gmail.com>
2013-03-04 17:37:12 +0800

    change username limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User
   has_secure_password
 
   validates :name, :email, :presence => true, :uniqueness => {:case_sensitive => false}
-  validates :name, :format => {:with => /\A\w+\z/, :message => 'only A-Z, a-z, _ allowed'}, :length => {:in => 6..20}
+  validates :name, :format => {:with => /\A\w+\z/, :message => 'only A-Z, a-z, _ allowed'}, :length => {:in => 4..20}
   validates :email, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
   validates :password, :password_confirmation, :presence => true, :on => :create
   validates :password, :length => {:minimum => 6, :allow_blank => true}
