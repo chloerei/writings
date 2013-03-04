1abf1d5f17b9dce035fbeacfe479247b7a1367f3
Rei <chloerei@gmail.com>
2013-03-04 15:37:13 +0800

    username length to 6..20

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:name){|n| "name#{n}" }
+    sequence(:name){|n| "username#{n}" }
     sequence(:email){|n| "email#{n}@codecampo.com" }
     password 'password'
     password_confirmation 'password'
