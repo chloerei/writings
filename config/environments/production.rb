c166aa563394e95116b34ad6ff95fc500f1b8d7a
Rei <chloerei@gmail.com>
2013-03-17 20:45:54 +0800

    change log_level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Publish::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
