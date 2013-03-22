82c156eab0de94d2004dd137118b5b5fcfdf744b
Rei <chloerei@gmail.com>
2013-03-22 20:03:16 +0800

    Revert "change log_level"
    This reverts commit c166aa563394e95116b34ad6ff95fc500f1b8d7a.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Publish::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :warn
+  # config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
