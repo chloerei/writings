329afdb260f670836e8c736c738cdbc2bff93bd9
Rei <chloerei@gmail.com>
2013-03-25 21:49:08 +0800

    fix admin articles tab count

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,15 +1,16 @@
 class Admin::ArticlesController < Admin::BaseController
   def index
-    @articles = case params[:tab]
-    when 'all'
-      Article.desc(:updated_at).includes(:user).page(params[:page]).per(25)
-    else
-      Article.publish.desc(:published_at).includes(:user).page(params[:page]).per(25)
+    @articles_scope = Article.scoped
+    if params[:name] && @user = User.where(:name => params[:name]).first
+      @articles_scope = @articles_scope.where(:user_id => @user.id)
     end
 
-    if params[:name] && user = User.where(:name => params[:name]).first
-      @articles = @articles.where(:user_id => user.id)
-    end
+    @articles = case params[:tab]
+                when 'all'
+                  @articles_scope.desc(:updated_at).includes(:user).page(params[:page]).per(25)
+                else
+                  @articles_scope.publish.desc(:published_at).includes(:user).page(params[:page]).per(25)
+                end
   end
 
   def show
