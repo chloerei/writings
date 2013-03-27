4af8d9a7cd4c7a96fcc39075b2a3d91a8b04e850
Rei <chloerei@gmail.com>
2013-03-27 16:56:45 +0800

    create billing action.

--- app/controllers/dashboard/billings_controller.rb
+++ app/controllers/dashboard/billings_controller.rb
@@ -1,4 +1,12 @@
 class Dashboard::BillingsController < Dashboard::BaseController
   def show
   end
+
+  def create
+    if params[:plan] == 'base' && current_user.plan_expired_at.blank?
+      current_user.update_attribute :plan, :base
+      current_user.update_attribute :plan_expired_at, 7.days.from_now
+    end
+    redirect_to dashboard_billing_url
+  end
 end
