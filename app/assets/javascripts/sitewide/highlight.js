c0d71a5c0c822a8d8f516a615930151faa2a9fa3
Rei <chloerei@gmail.com>
2013-04-01 20:27:11 +0800

    change pre code style

--- app/assets/javascripts/sitewide/highlight.js
+++ app/assets/javascripts/sitewide/highlight.js
@@ -1,5 +1,6 @@
 //= require highlight_js/highlight
 //= require highlight_js/languages/ruby
+//= require highlight_js/languages/python
 //= require highlight_js/languages/javascript
 //= require highlight_js/languages/bash
 //= require highlight_js/languages/xml
