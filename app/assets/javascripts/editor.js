fee6368e4bd0ee785793f7c24081851545c1cffc
Rei <chloerei@gmail.com>
2013-04-23 16:18:50 +0800

    don't trigger editor:change on backspace, it conflic with input method.

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -140,14 +140,6 @@ Editor.prototype = {
       case 8: // Backspace
       case 46: // Delete
         this.initParagraph();
-
-        // chrome and safari bind textInput, so here trigger editor:change.
-        // no support safari:
-        // bugfix https://github.com/writings-io/writings-io/issues/3#issuecomment-14832829
-        if (this.is_chrome) {
-          this.undoManager.save();
-          this.editable.trigger('editor:change');
-        }
         break;
     }
   },
