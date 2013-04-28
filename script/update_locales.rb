4f16c34d0aecd59540176ddd7e1e312ddf98e361
Rei <chloerei@gmail.com>
2013-04-28 19:52:48 +0800

    update en locales

--- script/update_locales.rb
+++ script/update_locales.rb
@@ -4,5 +4,5 @@ target = File.open("#{Rails.root}/config/locales/en.yml")
 target_yaml = YAML.load target
 
 File.open("#{Rails.root}/config/locales/en.yml", 'w') do |file|
-  file.write({:en => source_yaml['zh-CN'].deep_merge(target_yaml['en'])}.to_yaml)
+  file.write({'en' => source_yaml['zh-CN'].deep_merge(target_yaml['en'])}.to_yaml)
 end
