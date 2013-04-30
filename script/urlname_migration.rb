b1f423d8d162f9290447a7b94d39422ea6252a20
Rei <chloerei@gmail.com>
2013-04-30 19:27:10 +0800

    remove unuse script

--- script/urlname_migration.rb
+++
@@ -1,9 +0,0 @@
-Article.asc(:_id).each do |article|
-  urlname_was = article.urlname
-  if article.urlname.present?
-    article.update_attribute :urlname, "#{article.token}-#{article.urlname}"
-  else
-    article.update_attribute :urlname, article.token
-  end
-  puts "#{urlname_was} -> #{article.urlname}"
-end
