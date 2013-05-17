5b011bc403e9f28148eca96b949697a1cfe8e3cd
Rei <chloerei@gmail.com>
2013-05-17 17:23:10 +0800

    remove dashboard/dashboard.

--- app/controllers/dashboard/dashboard_controller.rb
+++
@@ -1,8 +0,0 @@
-class Dashboard::DashboardController < Dashboard::BaseController
-  skip_filter :require_logined, :only => :show
-
-  def show
-    @articles = @space.articles.status(nil).desc(:updated_at).limit(10)
-    @attachments = @space.attachments.desc(:created_at).limit(5)
-  end
-end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   layout 'dashboard'
 
   def index
+    if logined?
+      redirect_to dashboard_root_path(current_user)
+    end
   end
 end
