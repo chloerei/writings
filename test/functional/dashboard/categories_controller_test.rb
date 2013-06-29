083f936159f450188923f42e688ca7797ddd2483
Rei <chloerei@gmail.com>
2013-06-29 19:07:24 +0800

    fix template error

--- test/functional/dashboard/categories_controller_test.rb
+++ test/functional/dashboard/categories_controller_test.rb
@@ -9,26 +9,25 @@ class Dashboard::CategoriesControllerTest < ActionController::TestCase
 
   test "should create category" do
     assert_difference "@user.categories.count" do
-      post :create, :space_id => @user, :category => attributes_for(:category), :format => :json
+      post :create, :space_id => @user, :category => attributes_for(:category), :format => :js
       assert_response :success, @response.body
     end
   end
 
   test "should get edit page" do
-    get :edit, :space_id => @user, :id => @category
+    get :edit, :space_id => @user, :id => @category, :format => :js
     assert_response :success, @response.body
   end
 
   test "should update category" do
-    post :update, :space_id => @user, :id => @category, :category => { :name => 'change' }, :format => :json
+    post :update, :space_id => @user, :id => @category, :category => { :name => 'change' }, :format => :js
     assert_response :success, @response.body
     assert_equal 'change', @category.reload.name
   end
 
   test "should destroy category" do
     assert_difference "@user.categories.count", -1 do
-      delete :destroy, :space_id => @user, :id => @category
-      assert_redirected_to root_url
+      delete :destroy, :space_id => @user, :id => @category, :format => :js
     end
   end
 end
