6595532ce4fd42e8d04b866197a6cd838ccb2c5b
Rei <chloerei@gmail.com>
2013-07-07 15:53:35 +0800

    remove discussion topics comment migrate.

--- db/migrate/20130621092426_space_token_id.rb
+++
@@ -1,9 +0,0 @@
-class SpaceTokenId < Mongoid::Migration
-  def self.up
-    Comment.all.rename(:workspace_id, :space_id)
-    Discussion.all.rename(:workspace_id, :space_id)
-  end
-
-  def self.down
-  end
-end
