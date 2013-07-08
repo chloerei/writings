ec73090e875f450f429d76f053caad8e567e0b6f
Rei <chloerei@gmail.com>
2013-07-09 00:12:43 +0800

    remove old secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,8 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Publish::Application.config.secret_token = 'cd5330c10e83a9c91b4ba5011ebbcbe323fa24c0e1861cefad743da844148c18201d9006fbad534b8ec438e87cb1105a447047b033299f51b5f7e7369480a06e'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
 Publish::Application.config.secret_key_base = '71198825e963169a145ef28fd6de8c3edbb69172d4441982b670c0f4374167f4445b1e593cb950e892002c65fdca29bfb079425679e395dde0e0db36ffce387b'
