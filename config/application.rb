388681c52b40d492f56762268b9fbb7f11706ed3
Rei <chloerei@gmail.com>
2013-07-08 23:33:03 +0800

    remove unuse autoload paths.

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,6 @@ Bundler.require(:default, Rails.env)
 
 module Publish
   class Application < Rails::Application
-    # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(
-      #{config.root}/app/models/concerns
-    )
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Beijing'
