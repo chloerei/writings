f87b56ce68f239f6c93ecbbbe30a73c7131f8dee
Rei <chloerei@gmail.com>
2013-07-16 22:47:25 +0800

    logined_redirect fix in home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   layout 'dashboard_base'
+  before_filter :logined_redirect
 
-  def index
+  private
+
+  def logined_redirect
     if logined?
       redirect_to dashboard_root_path(current_user)
     end
