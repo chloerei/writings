74a033dd295fed682d8aff35e994ab7181d4d30b
Rei <chloerei@gmail.com>
2013-07-27 22:53:18 +0800

    fix migate

--- db/migrate/20130727095234_invoice_rename_to_orders.rb
+++ db/migrate/20130727095234_invoice_rename_to_orders.rb
@@ -1,6 +1,6 @@
 class InvoiceRenameToOrders < Mongoid::Migration
   def self.up
-    Mongoid.default_session.with(:db => 'admin').command :renameCollection => 'publish_production.invoices', :to => 'publish_production.orders'
+    Mongoid.default_session.with(:database => 'admin').command :renameCollection => 'publish_production.invoices', :to => 'publish_production.orders'
     Order.update_all(:state => 'accepted')
     Order.all.rename(:approved_at => :accepted_at)
   end
