519f7dbd7f6f3afe5a8500eb73eb6bf794cbb035
Rei <chloerei@gmail.com>
2013-08-06 00:57:16 +0800

    add recaptcha for login.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,15 +8,17 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    @user = User.where(:email => params[:email]).first
-    if @user
-      if !require_recaptcha? || verify_recaptcha(:model => @user)
+    if !require_recaptcha? || verify_recaptcha(:model => @user)
+      @user = User.where(:email => params[:email]).first
+      if @user
         @user.generate_password_reset_token
         SystemMailer.delay.password_reset(@user.id.to_s)
         flash[:info] = I18n.t(:password_reset_email_send)
+      else
+        flash[:error] = I18n.t(:password_reset_email_no_found)
       end
     else
-      flash[:error] = I18n.t(:password_reset_email_no_found)
+      flash[:error] = I18n.t('recaptcha.errors.verification_failed')
     end
   end
 
@@ -44,11 +46,11 @@ class PasswordResetsController < ApplicationController
   private
 
   def inc_ip_count
-    Rails.cache.write "password_resets_count/#{request.remote_ip}", ip_count + 1, :tti => 60.seconds
+    Rails.cache.write "password_reset_count/#{request.remote_ip}", ip_count + 1, :tti => 60.seconds
   end
 
   def ip_count
-    Rails.cache.read("password_resets_count/#{request.remote_ip}").to_i
+    Rails.cache.read("password_reset_count/#{request.remote_ip}").to_i
   end
 
   def require_recaptcha?
