9411113a852bc294241b991eaabbd24911273ec9
Rei <chloerei@gmail.com>
2013-08-14 15:10:53 +0800

    fix split user and space migration.

--- db/migrate/20130811083546_split_user_and_space.rb
+++ db/migrate/20130811083546_split_user_and_space.rb
@@ -5,12 +5,17 @@ class SplitUserAndSpace < Mongoid::Migration
     Invitation.all.rename :workspace_id => :space_id
 
     # split user
-    user_attributes = [:email, :password_digest, :password_reset_token, :password_reset_token_created_at, :locale, :created_at, :updated_at]
-    Space.where(:_type => 'User').asc(:_id).each do |space|
-      user = User.new(space.attributes.splice(user_attributes + [:_id] ))
-      user.save(:validate => true)
+    Space.where(:_type => 'Workspace').rename(:creator_id => :user_id)
+    Space.all.unset(:_type)
+    user_attributes = [:email, :name, :full_name, :description, :password_digest, :password_reset_token, :password_reset_token_created_at, :locale, :created_at, :updated_at]
+    Space.where(:user_id => nil).asc(:_id).each do |space|
+      user_attr = space.attributes.symbolize_keys.slice(*(user_attributes + [:_id]))
+      user = User.new(user_attr)
+      user.save(:validate => false)
+      space.update_attribute :user_id, user.id
+      space.members << user
     end
-    Space.all.unset(user_attributes + [:_type])
+    Space.all.unset(user_attributes - [:name, :full_name, :description])
 
     # Order
     Order.all.rename :user_id => :space_id
