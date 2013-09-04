68e0bbec12f319c01b3596715ce256bcca9083b5
Rei <chloerei@gmail.com>
2013-09-04 17:34:33 +0800

    en home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,16 @@
 class HomeController < ApplicationController
   layout 'home'
+  before_filter :logined_redirect
+
+  private
+
+  def logined_redirect
+    if logined?
+      if space = current_user.spaces.asc(:created_at).first
+        redirect_to dashboard_root_path(space)
+      else
+        redirect_to new_space_path
+      end
+    end
+  end
 end
